523054e368ac15e9402393ecf1f294defde4f9f4
Alexandr Yankovich <shurik.yankovich@gmail.com>
2020-05-27 17:29:08 +0300

    Changed Laptop class in exercise 3 in task 3.

--- src/task_3/exercise_3/product/Laptop.java
+++ src/task_3/exercise_3/product/Laptop.java
@@ -6,9 +6,14 @@ import task_3.exercise_3.product_part.Motherboard;
 import task_3.exercise_3.product_part.ProductPart;
 
 public class Laptop implements Product {
+    private LaptopCase laptopCase;
+    private Motherboard motherboard;
+    private Monitor monitor;
+
     @Override
     public void installFirstPart(ProductPart firstProductPart) {
         if (firstProductPart instanceof LaptopCase) {
+            laptopCase = (LaptopCase) firstProductPart;
             System.out.println("Установлен корпус ноутбука!");
         } else {
             System.out.println("Неверно подобрана деталь!");
@@ -18,6 +23,7 @@ public class Laptop implements Product {
     @Override
     public void installSecondPart(ProductPart secondProductPart) {
         if (secondProductPart instanceof Motherboard) {
+            motherboard = (Motherboard) secondProductPart;
             System.out.println("Установлена материнская плата ноутбука!");
         } else {
             System.out.println("Неверно подобрана деталь!");
@@ -27,6 +33,7 @@ public class Laptop implements Product {
     @Override
     public void installThirdPart(ProductPart thirdProductPart) {
         if (thirdProductPart instanceof Monitor) {
+            monitor = (Monitor) thirdProductPart;
             System.out.println("Установлен монитор ноутбука!");
         } else {
             System.out.println("Неверно подобрана деталь!");
