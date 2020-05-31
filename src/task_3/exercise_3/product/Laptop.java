1d5fa3281e6b800140eb84e58ea34d35f4defcb9
Alexandr Yankovich <shurik.yankovich@gmail.com>
2020-05-31 23:31:38 +0300

    Delete instanceof check and array ProductPart replaced by three variables in exercise 3.

--- src/task_3/exercise_3/product/Laptop.java
+++ src/task_3/exercise_3/product/Laptop.java
@@ -1,45 +1,28 @@
 package task_3.exercise_3.product;
 
-import task_3.exercise_3.product_part.LaptopCase;
-import task_3.exercise_3.product_part.Monitor;
-import task_3.exercise_3.product_part.Motherboard;
 import task_3.exercise_3.product_part.ProductPart;
 
 public class Laptop implements Product {
 
-    private ProductPart[] productParts;
-
-    public Laptop() {
-        productParts = new ProductPart[3];
-    }
+    private ProductPart firstProductParts;
+    private ProductPart secondProductPart;
+    private ProductPart thirdProductPart;
 
     @Override
     public void installFirstPart(ProductPart firstProductPart) {
-        if (firstProductPart instanceof LaptopCase) {
-            productParts[0] = firstProductPart;
-            System.out.println("Установлен корпус ноутбука!");
-        } else {
-            System.out.println("Неверно подобрана деталь!");
-        }
+        this.firstProductParts = firstProductPart;
+        System.out.println("Установлен корпус ноутбука!");
     }
 
     @Override
     public void installSecondPart(ProductPart secondProductPart) {
-        if (secondProductPart instanceof Motherboard) {
-            productParts[1] = secondProductPart;
-            System.out.println("Установлена материнская плата ноутбука!");
-        } else {
-            System.out.println("Неверно подобрана деталь!");
-        }
+        this.secondProductPart = secondProductPart;
+        System.out.println("Установлена материнская плата ноутбука!");
     }
 
     @Override
     public void installThirdPart(ProductPart thirdProductPart) {
-        if (thirdProductPart instanceof Monitor) {
-            productParts[2] = thirdProductPart;
-            System.out.println("Установлен монитор ноутбука!");
-        } else {
-            System.out.println("Неверно подобрана деталь!");
-        }
+        this.thirdProductPart = thirdProductPart;
+        System.out.println("Установлен монитор ноутбука!");
     }
 }
