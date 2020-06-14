6cda98239092d8019c46f91b6a20014047c2113f
Alexandr Yankovich <shurik.yankovich@gmail.com>
2020-06-14 21:57:18 +0300

    Added menu for sorting books.

--- src/electronicbookstore/menu/constant/BookListMenuTextConst.java
+++ src/electronicbookstore/menu/constant/BookListMenuTextConst.java
@@ -2,10 +2,10 @@ package electronicbookstore.menu.constant;
 
 public class BookListMenuTextConst {
 
-    public static final String ITEM_TEXT_BOOK_TITLE_SORT_ = "Работа со складом книг";
-    public static final String ITEM_TEXT_PUBLICATION_DATE_SORT = "Работа со складом книг";
-    public static final String ITEM_TEXT_BOOK_PRICE_SORT = "Работа со складом книг";
-    public static final String ITEM_TEXT_BOOK_PRESENCE_SORT = "Работа со складом книг";
-    public static final String ITEM_TEXT_WORK_WITH_STORAGE = "Работа со складом книг";
-    public static final String ITEM_TEXT_RETURN_TO_MAIN_MENU = "Работа со складом книг";
+    public static final String ITEM_TEXT_BOOK_TITLE_SORT = "Отсортировать список по названию книг";
+    public static final String ITEM_TEXT_PUBLICATION_DATE_SORT = "Отсортировать список по дате публикации книг";
+    public static final String ITEM_TEXT_BOOK_PRICE_SORT = "Отсортировать список по цене книг";
+    public static final String ITEM_TEXT_BOOK_PRESENCE_SORT = "Отсортировать список по наличию на складе";
+    public static final String ITEM_TEXT_BOOK_LIST_WITHOUT_SORT = "Вывести список без сортировки";
+    public static final String ITEM_TEXT_RETURN_TO_MAIN_MENU = "Вернуться в главное меню";
 }
--- src/electronicbookstore/menu/constant/UnsoldBookListMenuAction.java
+++ src/electronicbookstore/menu/constant/UnsoldBookListMenuAction.java
@@ -1,4 +1,8 @@
 package electronicbookstore.menu.constant;
 
 public enum UnsoldBookListMenuAction {
+    ARRIVAL_DATE_SORT,
+    BOOK_PRICE_SORT,
+    BOOK_LIST_WITHOUT_SORT,
+    RETURN_TO_MAIN_MENU
 }
