9b350ca9284ebdbcabe14b20d2597a9eca7caa3f
Alexandr Yankovich <shurik.yankovich@gmail.com>
2020-06-14 22:08:39 +0300

    Added menu for sorting requests.

---
+++ src/electronicbookstore/menu/constant/RequestListMenuAction.java
@@ -0,0 +1,9 @@
+package electronicbookstore.menu.constant;
+
+public enum RequestListMenuAction {
+
+    REQUEST_BOOK_NAME_SORT,
+    REQUEST_COUNT_SORT,
+    BOOK_LIST_WITHOUT_SORT,
+    RETURN_TO_MAIN_MENU
+}
---
+++ src/electronicbookstore/menu/constant/RequestListMenuTextConst.java
@@ -0,0 +1,12 @@
+package electronicbookstore.menu.constant;
+
+public class RequestListMenuTextConst {
+
+    public static final String ITEM_TEXT_REQUEST_BOOK_NAME_SORT = "Отсортировать список по названию книг";
+    public static final String ITEM_TEXT_REQUEST_COUNT_SORT = "Отсортировать список по количеству запросов";
+    public static final String ITEM_TEXT_BOOK_LIST_WITHOUT_SORT = "Вывести список без сортировки";
+    public static final String ITEM_TEXT_RETURN_TO_MAIN_MENU = "Вернуться в главное меню";
+
+    private RequestListMenuTextConst() {
+    }
+}
