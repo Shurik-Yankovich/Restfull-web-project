b285d37936a5c1a4084c9c20754290ceabcb88e7
Alexandr Yankovich <shurik.yankovich@gmail.com>
2020-09-24 00:14:03 +0300

    Added some tests for request service.

--- Bookstore/bookstore-service/src/test/java/service/request/BookRequestServiceTest.java
+++ Bookstore/bookstore-service/src/test/java/service/request/BookRequestServiceTest.java
@@ -3,7 +3,9 @@ package service.request;
 import config.TestConfig;
 import entity.Book;
 import entity.Request;
+import entity.Status;
 import exeption.RepositoryException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +14,10 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import repository.base.RequestRepository;
 import repository.file.FileRequestRepository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
@@ -27,16 +32,81 @@ public class BookRequestServiceTest {
     @Autowired
     private FileRequestRepository fileRequestRepository;
 
-    private static final Book book = mock(Book.class);
+    private static Book book;
+    private static List<Book> bookList;
+    private static Request expectedRequest;
+
+    @BeforeAll
+    public static void init(){
+        book = new Book("Алхимик","Пауло Коэльо",2019);
+        book.setId(1);
+        bookList = new ArrayList<>();
+        bookList.add(book);
+        expectedRequest = new Request(book);
+        expectedRequest.setCount(1);
+    }
 
     @Test
-    public void addRequest() throws RepositoryException {
-//        when(requestService.)
-        Request request = new Request(book);
-        request.setCount(1);
-        when(requestRepository.create(any(Request.class))).thenReturn(request);
-        Request request1 = requestService.addRequest(book);
-        assertEquals(request1, request);
+    public void addRequestForBookThatHasNoRequestsYet() throws RepositoryException {
+        when(requestRepository.create(any(Request.class))).thenReturn(expectedRequest);
+        Request actualRequest = requestService.addRequest(book);
+        assertEquals(expectedRequest, actualRequest);
     }
+
+    @Test
+    public void addRequestWithExceptionCreateInDatabase() {
+        Request actualRequest = requestService.addRequest(book);
+        assertNotNull(actualRequest);
+    }
+
+    @Test
+    public void addRequestListByBookWithOneBookOnList() throws RepositoryException {
+        when(requestRepository.create(any(Request.class))).thenReturn(expectedRequest);
+        List<Request> actualRequestList = requestService.addRequestList(bookList);
+        List<Request> expectedRequestList = new ArrayList<>();
+        expectedRequestList.add(expectedRequest);
+        assertEquals(expectedRequestList, actualRequestList);
+    }
+
+    @Test
+    public void completeRequestsByBookWhenThereIsOneRequestForThisBook() throws RepositoryException {
+        List<Request> databaseRequestList = new ArrayList<>();
+        databaseRequestList.add(new Request(book));
+        when(requestRepository.readAll()).thenReturn(databaseRequestList);
+        List<Request> actualRequestList = requestService.completeRequestsByBook(book);
+        assertEquals(Status.COMPLETED, actualRequestList.get(0).getStatus());
+    }
+
+    @Test
+    public void completeRequest() throws RepositoryException {
+        when(requestRepository.update(any(Request.class))).thenReturn(expectedRequest);
+        Request request = new Request(book);
+        Request actualRequest = requestService.completeRequest(request);
+        assertEquals(Status.COMPLETED, actualRequest.getStatus());
+    }
+
+    @Test
+    public void completeRequestWhenRequestIsNull() {
+        assertThrows(NullPointerException.class, () -> {
+            requestService.completeRequest(null);
+        });
+    }
+
+    @Test
+    public void cancelRequest() throws RepositoryException {
+        when(requestRepository.update(any(Request.class))).thenReturn(any(Request.class));
+        Request actualRequest = requestService.cancelRequest(new Request(book));
+        assertEquals(Status.CANCELED, actualRequest.getStatus());
+    }
+
+    @Test
+    public void cancelRequestWhenRequestIsNull() {
+        assertThrows(NullPointerException.class, () -> {
+            requestService.cancelRequest(null);
+        });
+    }
+
+
+
 
 }
