d223f2bfa28d40584e93bcbef7dd203a52557cfd
Alexandr Yankovich <shurik.yankovich@gmail.com>
2020-09-30 23:09:04 +0300

    Some refactoring.

--- Bookstore/bookstore-service/src/test/java/service/order/BookOrderServiceTest.java
+++ Bookstore/bookstore-service/src/test/java/service/order/BookOrderServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
