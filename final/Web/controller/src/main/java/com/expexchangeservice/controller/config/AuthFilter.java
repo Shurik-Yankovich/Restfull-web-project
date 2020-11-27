c2af34eb61c97f902b17bea7619c7d557d0dcb18
Shurik_Yankovich <shurikyankovich@gmail.com>
2020-11-27 16:13:22 +0300

    Changed Filter.

--- final/Web/controller/src/main/java/com/expexchangeservice/controller/config/AuthFilter.java
+++ final/Web/controller/src/main/java/com/expexchangeservice/controller/config/AuthFilter.java
@@ -1,7 +1,11 @@
 package com.expexchangeservice.controller.config;
 
+import com.expexchangeservice.model.entities.User;
+import com.expexchangeservice.model.entities.UserAuthentication;
 import com.expexchangeservice.service.impl.UserService;
+import com.expexchangeservice.utils.security.ITokenHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
@@ -14,22 +18,22 @@ import java.io.IOException;
 
 @Component
 public class AuthFilter extends GenericFilterBean {
-//    @Autowired
-//    private TokenHandler tokenHandler;
-//    @Autowired
-//    private UserService profileService;
+    @Autowired
+    private ITokenHandler tokenHandler;
+    @Autowired
+    private UserService userService;
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
-//        String token = httpRequest.getHeader(tokenHandler.AUTH_HEADER_NAME);
-//        UserAuthentication userAuth = null;
-//        if (tokenHandler.checkToken(token)) {
-//            UserProfile dbUser = profileService.findUserById(tokenHandler.getUserIdFromToken(token));
+        String token = httpRequest.getHeader(tokenHandler.AUTH_HEADER_NAME);
+        UserAuthentication userAuth = null;
+        if (tokenHandler.checkToken(token)) {
+            User dbUser = userService.loadUserById(tokenHandler.getUserIdFromToken(token));
 //            UserKeeper.setLoggedUser(dbUser);
-//            userAuth = new UserAuthentication(dbUser);
-//        }
-//        SecurityContextHolder.getContext().setAuthentication(userAuth);
+            userAuth = new UserAuthentication(dbUser);
+        }
+        SecurityContextHolder.getContext().setAuthentication(userAuth);
         chain.doFilter(request, response);
     }
 }
